f53b06e60b137baac5a1cf7d29f97ffec1819489
Vitória Viana <vitorialegal552@gmail.com>
2026-06-09 11:22:58 -0300

    somativa 09.06.25

--- LOGICA DE PROGRAMAÇAO/Somativas/soamtiva2.py
+++ LOGICA DE PROGRAMAÇAO/Somativas/soamtiva2.py
@@ -49,21 +49,55 @@
 # janela.mainloop()
 
 
-# Cálculo de Produção: Peça a quantidade de peças produzidas em 1 hora. Calcule e
+#2. Cálculo de Produção: Peça a quantidade de peças produzidas em 1 hora. Calcule e
 # exiba quantas peças serão produzidas em um turno de 8 horas.
+
+
+# import tkinter as tk
+# from tkinter import messagebox
+
+# def janela_info():
+#    peças =int(pecas_hora.get())
+
+#    if peças == "":
+#     messagebox.showwarning("Aviso" , "Digite quantas peças são produzidas por hora")
+#    else:
+#      total = peças * 8
+#      messagebox.showinfo("Calculado" ,  f"em 8 horas {total} , peças são produzidas ")
+
+
+# janela = tk.Tk()
+# janela.title("Exemplo 2")
+# janela.geometry("300x300")
+# janela.configure(bg="red")
+
+
+# lbl_peças= tk.Label(janela, text="Digite quantas peças são produzidas por hora: ")
+# lbl_peças.grid(row=0, column=0, pady=60, padx=10) 
+
+# pecas_hora= tk.Entry(janela, font=("Arial", 12))
+# pecas_hora.grid(row=1, column=0, pady=10, padx=10)
+
+# btn_Entre= tk.Button(janela, text="calcular" , command= janela_info)
+# btn_Entre.grid(row=3, column=0, pady=10, padx=10)
+
+# janela.mainloop()
+
+# 3. Conversor de Unidade: O sistema lê uma pressão em Bar. Converta para PSI (1 Bar
+# ≈ 14.5 PSI) e exiba com duas casas decimais.
 
 
 import tkinter as tk
 from tkinter import messagebox
 
 def janela_info():
-   peças = pecas_hora.get()
+   pressaobar =int(pressao_bar.get())
 
-   if peças == "":
-    messagebox.showwarning("Aviso" , "Digite quantas peças são produzidas por hora")
+   if pressaobar == "":
+    messagebox.showwarning("Aviso" ,"Digite a pressao em Bar")
    else:
-     total = total * 8
-     messagebox.showinfo(f"em 8 horas {total} , peças são produzidas ")
+     total = pressaobar * 14.5
+     messagebox.showinfo("Calculado" ,  f"em 8 horas {total} , peças são produzidas ")
 
 
 janela = tk.Tk()
@@ -77,13 +111,88 @@ lbl_peças.grid(row=0, column=0, pady=60, padx=10)
 
 pecas_hora= tk.Entry(janela, font=("Arial", 12))
 pecas_hora.grid(row=1, column=0, pady=10, padx=10)
-# btn_Entre= tk.Button(janela, text="Entre" , command= janela_info)
-# btn_Entre.grid(row=3, column=0, pady=10, padx=10)
 
-turno_operador = tk.Entry(janela, font=("Arial", 12))
-turno_operador.grid(row=1, column=1, pady=10, padx=10)
-
-lbl_operador = tk.Label(janela, text="Digite seu turno")
-lbl_operador.grid(row=1, column=0, pady=10, padx=10)
+btn_Entre= tk.Button(janela, text="calcular" , command= janela_info)
+btn_Entre.grid(row=3, column=0, pady=10, padx=10)
 
 janela.mainloop()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# 4. Média de Qualidade: Peça 3 notas de inspeção de uma peça (0 a 10). Exiba a média
+#aritmética simples delas.
+
+# import tkinter as tk
+# from tkinter import messagebox
+
+# def janela_notas():
+#    peça1 = int(peca_um.get())
+#    peça2 = int(peca_dois.get())
+#    peça3 = int(peca_tres.get())
+
+#    if peça1 == "" and peça2 == "" and peça3 == "":
+#       messagebox.showwarning("Digite uma nota para cada peça")
+#    else:
+#       soma = peça1 + peça2 + peça3
+#       total = soma / 3 
+#       messagebox.showinfo("Calculado" ,  f"A média das peças é de: {total}  ")
+      
+
+# janela = tk.Tk()
+# janela.title("Exemplo 2")
+# janela.geometry("600x600")
+# janela.configure(bg="green")
+
+
+# lbl_peça_um = tk.Label(janela, text="Dê uma nota de 0 a 10 para primeira peça")
+# lbl_peça_um.grid(row=0, column=0, pady=60, padx=10)
+
+# peca_um = tk.Entry(janela, font=("Arial", 12))
+# peca_um.grid(row=0, column=1, pady=10, padx=10)
+
+# lbl_peca_dois = tk.Label(janela, text="Dê uma nota de 0 a 10  para segunda peça")
+# lbl_peca_dois.grid(row=1, column=0, pady=10, padx=10)
+
+# peca_dois = tk.Entry(janela, font=("Arial", 12))
+# peca_dois.grid(row=1, column=1, pady=10, padx=10)
+
+# lbl_peca_tres = tk.Label(janela, text="Dê uma nota de 0 a 10 para terceira peça")
+# lbl_peca_tres.grid(row=2, column=0, pady=10, padx=10)
+
+# peca_tres= tk.Entry(janela, font=("Arial", 12))
+# peca_tres.grid(row=2, column=1, pady=10, padx=10)
+
+
+# btn_Entre= tk.Button(janela, text="Entre" , command= janela_notas)
+# btn_Entre.grid(row=4, column=0, pady=10, padx=10)
+
+# janela.mainloop()
